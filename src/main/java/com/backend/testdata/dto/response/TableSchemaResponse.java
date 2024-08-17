b9d08e2ed42cf30e66d3f4fc2a86d8502dd0ee35
hoonseung <dltmdgns4029@gmail.com>
2024-08-18 01:39:48 +0900

    # 28 TableSchemaResponse DTO 필드 순서 변경
    userId 순서를 변경함 엔티티 순서랑 동일하게 함

--- src/main/java/com/backend/testdata/dto/response/TableSchemaResponse.java
+++ src/main/java/com/backend/testdata/dto/response/TableSchemaResponse.java
@@ -5,15 +5,15 @@ import com.backend.testdata.dto.TableSchemaDto;
 import java.util.List;
 
 public record TableSchemaResponse(
-        String userId,
         String schemaName,
+        String userId,
         List<SchemaFieldResponse> schemaFields
 ) {
 
     public static TableSchemaResponse toResponse(TableSchemaDto dto) {
         return new TableSchemaResponse(
-                dto.userId(),
                 dto.schemaName(),
+                dto.userId(),
                 dto.schemaFields().stream()
                         .map(SchemaFieldResponse::toResponse)
                         .toList()

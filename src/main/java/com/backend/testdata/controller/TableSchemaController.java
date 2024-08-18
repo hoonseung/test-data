08c3e38685ddc183dfe5ecdd3e5ed7dbd9210ee8
hoonseung <dltmdgns4029@gmail.com>
2024-08-18 22:47:39 +0900

    #32 - 테이블 스키마 뷰 메인 페이지 부분 Controller 구현 및 테스트
    로그인이 필요없는 메인 페이지 접속 시 보여주는 테이블 스키마 뷰에 대해 Controller 테스트를 통과하기 위해 구현하고 table-schema.html 을 작성함

--- src/main/java/com/backend/testdata/controller/TableSchemaController.java
+++ src/main/java/com/backend/testdata/controller/TableSchemaController.java
@@ -1,55 +1,98 @@
 package com.backend.testdata.controller;
 
+import com.backend.testdata.domain.constants.ExportFileType;
+import com.backend.testdata.domain.constants.MockDataType;
+import com.backend.testdata.dto.request.TableSchemaExportRequest;
 import com.backend.testdata.dto.request.TableSchemaRequest;
+import com.backend.testdata.dto.response.SchemaFieldResponse;
+import com.backend.testdata.dto.response.TableSchemaResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@RequiredArgsConstructor
 @RequestMapping("/table-schema")
 @Controller
 public class TableSchemaController {
 
+  private final ObjectMapper mapper;
 
-    @GetMapping
-    public String tableSchema(@ModelAttribute TableSchemaRequest tableSchemaRequest) {
-        return "table-schema";
+
+  @GetMapping
+  public String tableSchema(@ModelAttribute TableSchemaRequest tableSchemaRequest, Model model) {
+    var tableSchema = defaultTableSchema();
+    model.addAttribute("tableSchema", tableSchema);
+    model.addAttribute("mockDataTypes", MockDataType.toObjects());
+    model.addAttribute("fileTypes", ExportFileType.toObjects());
+
+    return "table-schema";
+  }
+
+
+  @PostMapping
+  public String createOrUpdateTableSchema(
+      @ModelAttribute TableSchemaRequest tableSchemaRequest,
+      RedirectAttributes redirectAttributes) {
+
+    redirectAttributes.addFlashAttribute("tableSchemaRequest", tableSchemaRequest);
+
+    return "redirect:/table-schema";
+  }
+
+
+  @GetMapping("/my-schemas")
+  public String mySchemas() {
+
+    return "my-schemas";
+  }
+
+
+  @PostMapping("/my-schemas/{schemaName}")
+  public String deleteMySchema(
+      @PathVariable(name = "schemaName") String schemaName,
+      RedirectAttributes redirectAttributes) {
+
+    return "redirect:/table-schema/my-schemas";
+  }
+
+
+  @GetMapping("/export")
+  public ResponseEntity<String> exportTableSchema(
+      @ModelAttribute TableSchemaExportRequest tableSchemaExportRequest) {
+
+    return ResponseEntity.ok()
+        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=table-schema.txt")
+        .body(json(tableSchemaExportRequest));
+  }
+
+
+  private TableSchemaResponse defaultTableSchema() {
+    return new TableSchemaResponse(
+        "schema_name",
+        "shlee",
+        List.of(
+            new SchemaFieldResponse("fieldName1", MockDataType.STRING, 1, 0, null, null),
+            new SchemaFieldResponse("fieldName2", MockDataType.NUMBER, 2, 0, null, null),
+            new SchemaFieldResponse("fieldName3", MockDataType.NAME, 3, 0, null, null))
+    );
+  }
+
+  private String json(Object obj) {
+    try {
+      return mapper.writeValueAsString(obj);
+    } catch (JsonProcessingException jpe) {
+      throw new RuntimeException(jpe);
     }
-
-
-    @PostMapping
-    public String createOrUpdateTableSchema(
-            @ModelAttribute TableSchemaRequest tableSchemaRequest,
-            RedirectAttributes redirectAttributes) {
-
-        redirectAttributes.addFlashAttribute("tableSchemaRequest", tableSchemaRequest);
-
-        return "redirect:/table-schema";
-    }
-
-
-    @GetMapping("/my-schemas")
-    public String mySchemas() {
-
-        return "my-schemas";
-    }
-
-
-    @PostMapping("/my-schemas/{schemaName}")
-    public String deleteMySchema(
-            @PathVariable(name = "schemaName") String schemaName,
-            RedirectAttributes redirectAttributes) {
-
-        return "redirect:/table-schema/my-schemas";
-    }
-
-
-    @GetMapping("/export")
-    public ResponseEntity<String> exportTableSchema(@ModelAttribute TableSchemaRequest tableSchemaRequest) {
-
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=table-schema.txt")
-                .body("download complete!");
-    }
+  }
 }
--- src/test/java/com/backend/testdata/controller/TableSchemaControllerTest.java
+++ src/test/java/com/backend/testdata/controller/TableSchemaControllerTest.java
@@ -1,10 +1,25 @@
 package com.backend.testdata.controller;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.flash;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 import com.backend.testdata.configuration.SecurityConfiguration;
+import com.backend.testdata.domain.constants.ExportFileType;
 import com.backend.testdata.domain.constants.MockDataType;
+import com.backend.testdata.dto.request.TableSchemaExportRequest;
 import com.backend.testdata.util.FormDataEncoder;
 import com.backend.testdata.util.SchemaFieldRequestWithMock;
 import com.backend.testdata.util.TableSchemaRequestWithMock;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,94 +30,98 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.List;
-
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 @DisplayName("[Controller] 테이블 스키마")
 @Import({SecurityConfiguration.class, FormDataEncoder.class})
 @AutoConfigureMockMvc
 @WebMvcTest
 record TableSchemaControllerTest(
-        @Autowired MockMvc mvc,
-        @Autowired FormDataEncoder encoder
+    @Autowired MockMvc mvc,
+    @Autowired FormDataEncoder encoder,
+    @Autowired ObjectMapper mapper
 ) {
 
-    @DisplayName("[GET] 테이블 스키마 페이지를 요청하면 테이블 스키마 뷰 (정상) 를 반환한다.")
-    @Test
-    void whenEnteredTableSchemaPage_ThenShowTableSchemaView() throws Exception {
-        //given
+  @DisplayName("[GET] 테이블 스키마 페이지를 요청하면 테이블 스키마 뷰 (정상) 를 반환한다.")
+  @Test
+  void whenEnteredTableSchemaPage_ThenShowTableSchemaView() throws Exception {
+    //given
 
-        //when & then
-        mvc.perform(get("/table-schema"))
-                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-                .andExpect(status().isOk())
-                .andExpect(view().name("table-schema"));
-    }
-
-
-    @DisplayName("[POST] 테이블 스키마 생성, 변경을 요청하면 정상적으로 수행 후 테이블 스키마 페이지로 리다이렉션한다.")
-    @Test
-    void whenCreatedOrUpdatedTableSchema_ThenRedirectionToTableSchemaPage() throws Exception {
-        //given
-        var request = TableSchemaRequestWithMock.create("test_schema", "홍길동",
-                List.of(
-                        SchemaFieldRequestWithMock.create("id", MockDataType.ROW_NUMBER, 1, 0, null, null),
-                        SchemaFieldRequestWithMock.create("name", MockDataType.NAME, 2, 0, null, null),
-                        SchemaFieldRequestWithMock.create("age", MockDataType.NUMBER, 3, 0, null, null)
-                ));
+    //when & then
+    mvc.perform(get("/table-schema"))
+        .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+        .andExpect(status().isOk())
+        .andExpect(model().attributeExists("tableSchema"))
+        .andExpect(model().attributeExists("mockDataTypes"))
+        .andExpect(model().attributeExists("fileTypes"))
+        .andExpect(view().name("table-schema"));
+  }
 
 
-        //when & then
-        mvc.perform(post("/table-schema")
-                        .content(encoder.encode(request))
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-                        .with(csrf()))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(flash().attributeExists("tableSchemaRequest"))
-                .andExpect(redirectedUrl("/table-schema"));
-    }
+  @DisplayName("[POST] 테이블 스키마 생성, 변경을 요청하면 정상적으로 수행 후 테이블 스키마 페이지로 리다이렉션한다.")
+  @Test
+  void whenCreatedOrUpdatedTableSchema_ThenRedirectionToTableSchemaPage() throws Exception {
+    //given
+    var request = TableSchemaRequestWithMock.create("test_schema", "홍길동",
+        List.of(
+            SchemaFieldRequestWithMock.create("id", MockDataType.ROW_NUMBER, 1, 0, null, null),
+            SchemaFieldRequestWithMock.create("name", MockDataType.NAME, 2, 0, null, null),
+            SchemaFieldRequestWithMock.create("age", MockDataType.NUMBER, 3, 0, null, null)
+        ));
+
+    //when & then
+    mvc.perform(post("/table-schema")
+            .content(encoder.encode(request))
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+            .with(csrf()))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(flash().attributeExists("tableSchemaRequest"))
+        .andExpect(redirectedUrl("/table-schema"));
+  }
 
 
-    @DisplayName("[GET] 회원의 테이블 스키마 목록 페이지를 요청하면 회원의 테이블 스키마 목록 뷰 (정상) 를 반환한다.")
-    @Test
-    void whenAuthUserEnteredAllTableSchemaPage_ThenShowAllTableSchemaView() throws Exception {
-        //given
+  @DisplayName("[GET] 회원의 테이블 스키마 목록 페이지를 요청하면 회원의 테이블 스키마 목록 뷰 (정상) 를 반환한다.")
+  @Test
+  void whenAuthUserEnteredAllTableSchemaPage_ThenShowAllTableSchemaView() throws Exception {
+    //given
 
-        //when & then
-        mvc.perform(get("/table-schema/my-schemas"))
-                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-                .andExpect(status().isOk())
-                .andExpect(view().name("my-schemas"));
-    }
-
-
-    @DisplayName("[POST] 회원 스키마 삭제 (정상)")
-    @Test
-    void whenAuthUserDeleted_ThenRedirectionToUserTableSchemaView() throws Exception {
-        //given
-        String schemaName = "schemaName";
-        //when & then
-        mvc.perform(post("/table-schema/my-schemas/{schemaName}", schemaName)
-                        .with(csrf()))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/table-schema/my-schemas"));
-    }
+    //when & then
+    mvc.perform(get("/table-schema/my-schemas"))
+        .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+        .andExpect(status().isOk())
+        .andExpect(view().name("my-schemas"));
+  }
 
 
-    @DisplayName("[GET] 테이블 스키마 파일 다운로드 (정상) 후 파일을 반환한다.")
-    @Test
-    void whenTableSchemaDownLoading_ThenReturnResultFile() throws Exception {
-        //given
+  @DisplayName("[POST] 회원 스키마 삭제 (정상)")
+  @Test
+  void whenAuthUserDeleted_ThenRedirectionToUserTableSchemaView() throws Exception {
+    //given
+    String schemaName = "schemaName";
+    //when & then
+    mvc.perform(post("/table-schema/my-schemas/{schemaName}", schemaName)
+            .with(csrf()))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(redirectedUrl("/table-schema/my-schemas"));
+  }
 
-        //when & then
-        mvc.perform(get("/table-schema/export"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_PLAIN))
-                .andExpect(header().string(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=table-schema.txt"))
-                .andExpect(content().string("download complete!")); // TODO 수정 필요
-    }
+
+  @DisplayName("[GET] 테이블 스키마 파일 다운로드 (정상) 후 파일을 반환한다.")
+  @Test
+  void whenTableSchemaDownLoading_ThenReturnResultFile() throws Exception {
+    //given
+    var request = TableSchemaExportRequest.of("test", 60, ExportFileType.JSON,
+        List.of(
+            SchemaFieldRequestWithMock.create("id", MockDataType.ROW_NUMBER, 1, 0, null, null),
+            SchemaFieldRequestWithMock.create("name", MockDataType.NAME, 2, 0, null, null),
+            SchemaFieldRequestWithMock.create("age", MockDataType.NUMBER, 3, 0, null, null)
+        ));
+    var queryParam = encoder.encode(request, false);
+
+    //when & then
+    mvc.perform(get("/table-schema/export?" + queryParam))
+        .andExpect(status().isOk())
+        .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_PLAIN))
+        .andExpect(header().string(HttpHeaders.CONTENT_DISPOSITION,
+            "attachment; filename=table-schema.txt"))
+        .andExpect(content().json(mapper.writeValueAsString(request))); // TODO 수정 필요
+  }
 }

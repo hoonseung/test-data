79e6abf1f8031adbfa067ce209aa47f1042eb7bf
hoonseung <dltmdgns4029@gmail.com>
2024-08-19 21:48:34 +0900

    #35 - 내 테이블 스키마 목록 데이터에 포함되기 위해 Response DTO 수정일 추가
    내 테이블 스키마 목록에서 수정일시도 보여주기 위해 추가함

--- src/main/java/com/backend/testdata/dto/response/SimpleTableSchemaResponse.java
+++ src/main/java/com/backend/testdata/dto/response/SimpleTableSchemaResponse.java
@@ -1,16 +1,19 @@
 package com.backend.testdata.dto.response;
 
 import com.backend.testdata.dto.TableSchemaDto;
+import java.time.LocalDateTime;
 
 public record SimpleTableSchemaResponse(
-        String userId,
-        String schemaName
+    String userId,
+    String schemaName,
+    LocalDateTime modifiedAt
 ) {
 
-    public static SimpleTableSchemaResponse toSimpleResponse(TableSchemaDto dto) {
-        return new SimpleTableSchemaResponse(
-                dto.userId(),
-                dto.schemaName()
-        );
-    }
+  public static SimpleTableSchemaResponse toSimpleResponse(TableSchemaDto dto) {
+    return new SimpleTableSchemaResponse(
+        dto.userId(),
+        dto.schemaName(),
+        dto.modifiedAt()
+    );
+  }
 }
